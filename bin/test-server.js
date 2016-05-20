cb57f562d75f20bd1de1625f530f93f286af2e5c
Ant Cosentino <antony@mycosentino.com>
2016-05-20 10:44:08 +0200

    updates test server for disconnect request test

--- bin/test-server.js
+++ bin/test-server.js
@@ -4,8 +4,6 @@
 //
 // shared secret -- abcdefg
 
-var validator = require('validator')
-
 var radius = require('../')
 var server = new radius(
   'abcdefg',
@@ -21,19 +19,24 @@ server.on('Access-Request', function(accessrequest, rinfo) {
   if (accessrequest.attributes['User-Name'] === 'test' &&
       accessrequest.attributes['User-Password'] === 'test') {
     server.respond('auth', accessrequest, 'Access-Accept', rinfo, [], [], function() {});
-    setTimeout(function() {
-      server.send('coa', 'Disconnect-Request', rinfo, [
-        // Acct-Session-Id = "D91FE8E51802097"
-        // User-Name = "somebody"
-        // NAS-IP-Address = 10.0.0.2
-      ], [], console.log.bind(console))
-    }, 1000 * 5)
   } else {
     server.respond('auth', accessrequest, 'Access-Reject', rinfo, [], [], function() {});
   }
 }).on('Accounting-Request-Start', function(startrequest, rinfo) {
   // console.dir(startrequest.attributes, rinfo)
   server.respond('acct', startrequest, 'Accounting-Response', rinfo, [], [], function() {});
+  setTimeout(function() {
+    rinfo.port = server.COA_PORT
+    console.log('sending a disconnect-message', rinfo)
+    server.send('coa', 'Disconnect-Request', {
+      address: rinfo.address,
+      port: rinfo.port
+    }, [
+      ['Acct-Session-Id', startrequest.attributes['Acct-Session-Id']],
+      ['User-Name', startrequest.attributes['User-Name']],
+      ['NAS-IP-Address', startrequest.attributes['NAS-IP-Address']]
+    ], [], console.log.bind(console))
+  }, 1000 * 5)
 }).on('Accounting-Request-Interim-Update', function(interimrequest, rinfo) {
   // console.dir(interimrequest.attributes, rinfo)
   server.respond('acct', interimrequest, 'Accounting-Response', rinfo, [], [], function() {});
