e0d4ce8e5ad4ef2bae4cb3ffd2f4f6a1c0087288
Ant Cosentino <antony@mycosentino.com>
2017-09-15 11:16:41 +0200

    consistent style for tests

--- test/index.js
+++ test/index.js
@@ -1,9 +1,9 @@
 
-'use strict';
+'use strict'
 
 // ensures this library doesn't pass messages to itself
 function send(type, packet, done) {
-  var socket = dgram.createSocket('udp4');
+  var socket = dgram.createSocket('udp4')
   socket.bind(9000, socket.send.bind(
     socket,
     packet,
@@ -12,154 +12,174 @@ function send(type, packet, done) {
     type === 'acct' ? 1813 : 1812,
     '0.0.0.0',
     socket.close.bind(socket, done || function() {})
-  ));
+  ))
 }
 
-const readFileSync = require('fs').readFileSync;
-const join = require('path').join;
-const dgram = require('dgram');
+var readFileSync = require('fs').readFileSync
+var join = require('path').join
+var dgram = require('dgram')
 
-const expect = require('chai').expect;
+var expect = require('chai').expect
 
-const tephra = require('../');
+var tephra = require('../')
 
-const packets = {
+var packets = {
   auth: {
-    mangled: readFileSync(__dirname + '/packets/mikrotik/mangled.auth.packet'),
-    healthy: readFileSync(__dirname + '/packets/mikrotik/auth.packet')
+    mangled: readFileSync('./test/packets/mikrotik/mangled.auth.packet'),
+    healthy: readFileSync('./test/packets/mikrotik/auth.packet')
   },
   acct: {
-    mangled: readFileSync(__dirname + '/packets/mikrotik/mangled.acct.packet'),
-    healthy: readFileSync(__dirname + '/packets/mikrotik/acct.packet'),
-    start: readFileSync(__dirname + '/packets/mikrotik/Accounting_Request_Start.packet'),
-    interimUpdate: readFileSync(__dirname + '/packets/mikrotik/Accounting_Request_Interim_Update.packet'),
-    stop: readFileSync(__dirname + '/packets/mikrotik/Accounting_Request_Stop.packet')
+    mangled: readFileSync('./test/packets/mikrotik/mangled.acct.packet'),
+    healthy: readFileSync('./test/packets/mikrotik/acct.packet'),
+    start: readFileSync('./test/packets/mikrotik/Accounting_Request_Start.packet'),
+    interim_update: readFileSync('./test/packets/mikrotik/Accounting_Request_Interim_Update.packet'),
+    stop: readFileSync('./test/packets/mikrotik/Accounting_Request_Stop.packet')
   }
-};
+}
 
 describe('tephra', function() {
 
   describe('lifecycle', function() {
 
-    var server;
+    var server
 
     it('#constructor should throw if required arguments are missing', function() {
       expect(function() {
-        new tephra();
-      }).to.throw(/Missing SHARED_SECRET/);
-    });
+        new tephra()
+      }).to.throw(/Missing SHARED_SECRET/)
+    })
 
     it('#constructor should throw if vendor_id is missing when dictionary_path is present', function() {
       expect(function() {
         new tephra(
           'c33kr1t',
-          1812, 1813, 1814,
-          `${__dirname}/dictionaries/mikrotik.dictionary`
-        );
-      }).to.throw(/argument VENDOR_ID/);
-    });
+          1812,
+          1813,
+          1814,
+          './test/dictionaries/mikrotik.dictionary'
+        )
+      }).to.throw(/argument VENDOR_ID/)
+    })
 
     server = new tephra(
       'c33kr1t',
-      1812, 1813, 1814,
-      `${__dirname}/dictionaries/mikrotik.dictionary`,
+      1812,
+      1813,
+      1814,
+      './test/dictionaries/mikrotik.dictionary',
       14988
-    );
+    )
 
     it('sockets should bind', function(done) {
-      server.bind(done);
-    });
+      server.bind(done)
+    })
 
     it('sockets should unbind', function(done) {
-      server.unbind(done);
-    });
+      server.unbind(done)
+    })
 
-  });
+  })
 
   describe('auth, acct and coa packet transmission', function() {
 
-    var server;
+    var server
 
     beforeEach(function(done) {
       server = new tephra(
         'c33kr1t',
-        1812, 1813, 1814,
-        `${__dirname}/dictionaries/mikrotik.dictionary`,
+        1812,
+        1813,
+        1814,
+        './test/dictionaries/mikrotik.dictionary',
         14988
-      );
-      server.bind(done);
-    });
+      )
+      server.bind(done)
+    })
 
     afterEach(function(done) {
       server.unbind(done)
     })
 
     it('should handle invalid auth request packets gracefully', function(done) {
-      server.on('error#decode#auth', done.bind(done, null));
-      send('auth', packets.auth.mangled);
-    });
+      server.on('error#decode#auth', done.bind(done, null))
+      send('auth', packets.auth.mangled)
+    })
 
     it('should emit Access-Request object on receiving packet', function(done) {
-      server.on('Access-Request', done.bind(done, null));
-      send('auth', packets.auth.healthy);
-    });
+      server.on('Access-Request', done.bind(done, null))
+      send('auth', packets.auth.healthy)
+    })
 
     it('should handle invalid acct request packets gracefully', function(done) {
-      server.on('error#decode#acct', done.bind(done, null));
-      send('acct', packets.acct.mangled);
-    });
+      server.on('error#decode#acct', done.bind(done, null))
+      send('acct', packets.acct.mangled)
+    })
 
     it('should send a response correctly for accounting packets', function(done) {
       server.on('Accounting-Request-Interim-Update', function(request, rinfo) {
-        server.respond('acct', request, 'Accounting-Response', rinfo, [], [], done);
-      });
-      send('acct', packets.acct.interimUpdate);
-    });
+        server.respond('acct', request, 'Accounting-Response', rinfo, [], [], done)
+      })
+      send('acct', packets.acct.interim_update)
+    })
 
     it('should send a response correctly for accounting packets using the event handler responder function', function(done) {
       server.on('Accounting-Request-Interim-Update', function(request, rinfo, respond) {
-        respond([], [], done);
-      });
-      send('acct', packets.acct.interimUpdate);
-    });
+        respond([], [], done)
+      })
+      send('acct', packets.acct.interim_update)
+    })
 
     it('should send a response correctly for access-request packets', function(done) {
       server.on('Access-Request', function(request, rinfo) {
-        server.respond('auth', request, 'Access-Accept', rinfo, [], [], done);
-      });
-      send('auth', packets.auth.healthy);
-    });
+        server.respond('auth', request, 'Access-Accept', rinfo, [], [], done)
+      })
+      send('auth', packets.auth.healthy)
+    })
 
     it('should send a response correctly for access-request packets using the event handler responder function', function(done) {
       server.on('Access-Request', function(request, rinfo, accept, reject) {
-        accept([], [], done);
-      });
-      send('auth', packets.auth.healthy);
-    });
+        accept([], [], done)
+      })
+      send('auth', packets.auth.healthy)
+    })
 
     it('#send throw if supplied non-string type', function() {
-      expect(server.send).to.throw(/string argument type/);
-    });
+      expect(server.send).to.throw(/string argument type/)
+    })
 
     it('#send yield an error if supplied non-array type', function(done) {
-      server.send('acct', 0, {address: '0.0.0.0', port: 12345}, null, null, function(err) {
-        expect(err).to.be.ok;
-        done();
-      });
-    });
+      server.send(
+        'acct',
+        0,
+        {address: '0.0.0.0', port: 12345},
+        null,
+        null,
+        function(err) {
+          expect(err).to.be.ok
+          done()
+        }
+      )
+    })
 
     it('#respond throw if supplied non-string type', function() {
-      expect(server.respond).to.throw(/string argument type/);
-    });
+      expect(server.respond).to.throw(/string argument type/)
+    })
 
     it('#respond yield an error if no packet is given', function(done) {
-      // type, packet, code, rinfo, attributes, vendorAttributes, onResponded
-      server.respond('acct', null, 0, {address: '0.0.0.0', port: 12345}, null, null, function(err) {
-        expect(err).to.be.ok;
-        done();
-      })
-    });
+      server.respond(
+        'acct',
+        null,
+        0,
+        {address: '0.0.0.0', port: 12345},
+        null,
+        null,
+        function(err) {
+          expect(err).to.be.ok
+          done()
+        }
+      )
+    })
 
-  });
+  })
 
-});
+})
