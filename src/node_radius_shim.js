8e1cc5756f3dbb92b104b8495722677b9e29b38e
Ant Cosentino <skibz@users.noreply.github.com>
2022-06-03 15:11:01 +0200

    adjust vsa formatting (#36)
    Co-authored-by: Ant Cosentino <antony@mycosentino.com>

--- src/node_radius_shim.js
+++ src/node_radius_shim.js
@@ -10,11 +10,17 @@ module.exports = function node_radius_shim(
   Object.keys(
     vendor_attributes
   ).forEach(vendor_name => {
-    shimmed.push([
-      'Vendor-Specific',
-      this.VENDOR_IDS[vendor_name],
+
+    Object.keys(
       vendor_attributes[vendor_name]
-    ])
+    ).forEach(attr => {
+      shimmed.push([
+        'Vendor-Specific',
+        this.VENDOR_IDS[vendor_name],
+        [[attr, vendor_attributes[vendor_name][attr]]]
+      ])
+    })
+
   })
   return shimmed
 }
