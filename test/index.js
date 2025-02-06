5d14163640464136291545e0a643a6da46892a9e
Ant Cosentino <skibz@users.noreply.github.com>
2025-02-06 14:23:11 +0200

    checkpoint

--- test/index.js
+++ test/index.js
@@ -27,11 +27,19 @@ var auth_request = 'User-Name=foo,User-Password=bar'
 var acct_interim = 'Acct-Status-Type=Interim-Update'
 var acct_start = 'Acct-Status-Type=Start'
 var acct_stop = 'Acct-Status-Type=Stop'
-var coa_disconnect = 'Acct-Session-Id=foo,User-Name=bar,NAS-IP-Address=10.0.0.1'
+var port_permutations = [
+  [1812, 1813, 1814],
+  [1812, 1813, false],
+  [1812, false, 1814],
+  [1812, false, false],
+  [false, 1813, 1814],
+  [false, 1813, false],
+  [false, false, 1814]
+]
 
 describe('tephra', function() {
 
-  this.timeout(5000)
+  this.timeout(2000)
 
   describe('constructor', function() {
 
@@ -104,24 +112,14 @@ describe('tephra', function() {
 
   describe('socket permutations (authentication, accounting, change of authorisation)', function() {
 
-    var test_cases = [
-      [1812, 1813, 1814],
-      [1812, 1813, false],
-      [1812, false, 1814],
-      [1812, false, false],
-      [false, 1813, 1814],
-      [false, 1813, false],
-      [false, false, 1814]
-    ]
-
-    test_cases.forEach(function(test_case, idx) {
-      it(`permutation ${idx + 1} (${JSON.stringify(test_case)}) should bind and unbind successfully`, function(done) {
+    port_permutations.forEach(function(ports, idx) {
+      it(`permutation ${idx + 1} (${JSON.stringify(ports)}) should bind and unbind successfully`, function(done) {
         var t = new tephra({
           shared_secret: test_secret,
           ports: {
-            auth: test_case[0],
-            acct: test_case[1],
-            coa: test_case[2]
+            auth: ports[0],
+            acct: ports[1],
+            coa: ports[2]
           }
         })
 
@@ -149,7 +147,8 @@ describe('tephra', function() {
 
         server.bind(done)
       } catch (e) {
-        return done(e)
+        done(e)
+        return
       }
     })
 
@@ -167,7 +166,10 @@ describe('tephra', function() {
         test_secret,
         acct_start,
         function(err) {
-          if (err && !err.killed) return done(err)
+          if (err && !err.killed) {
+            done(err)
+            return
+          }
         }
       )
     })
@@ -181,7 +183,10 @@ describe('tephra', function() {
         test_secret,
         auth_request,
         function(err) {
-          if (err && !err.killed) return done(err)
+          if (err && !err.killed) {
+            done(err)
+            return
+          }
         }
       )
     })
@@ -196,7 +201,10 @@ describe('tephra', function() {
         test_secret,
         auth_request,
         function(err) {
-          if (err && !err.killed) return done(err)
+          if (err && !err.killed) {
+            done(err)
+            return
+          }
         }
       )
     })
@@ -212,7 +220,10 @@ describe('tephra', function() {
         test_secret,
         acct_interim,
         function(err) {
-          if (err && !err.killed) return done(err)
+          if (err && !err.killed) {
+            done(err)
+            return
+          }
         }
       )
     })
@@ -228,22 +239,47 @@ describe('tephra', function() {
         test_secret,
         acct_interim,
         function(err) {
-          if (err && !err.killed) return done(err)
+          if (err && !err.killed) {
+            done(err)
+            return
+          }
         }
       )
     })
 
     it('should emit the accounting request status type when receiving an accounting request', function(done) {
       var emissions = 0
-      var expected = 2
+      var expected = 6
 
       function emission_counter() {
         emissions += 1
-        if (emissions === expected) return done()
+        if (emissions === expected) {
+          done()
+          return
+        }
       }
 
+
+      // expected to be emitted three times (once per accounting status type)
       server.on('Accounting-Request', emission_counter)
+
+      // each expected to be emitted just once
+      server.on('Accounting-Request-Start', emission_counter)
       server.on('Accounting-Request-Interim-Update', emission_counter)
+      server.on('Accounting-Request-Stop', emission_counter)
+
+      radclient(
+        'localhost:1813',
+        'acct',
+        test_secret,
+        acct_start,
+        function(err) {
+          if (err && !err.killed) {
+            done(err)
+            return
+          }
+        }
+      )
 
       radclient(
         'localhost:1813',
@@ -251,7 +287,23 @@ describe('tephra', function() {
         test_secret,
         acct_interim,
         function(err) {
-          if (err && !err.killed) return done(err)
+          if (err && !err.killed) {
+            done(err)
+            return
+          }
+        }
+      )
+
+      radclient(
+        'localhost:1813',
+        'acct',
+        test_secret,
+        acct_stop,
+        function(err) {
+          if (err && !err.killed) {
+            done(err)
+            return
+          }
         }
       )
     })
@@ -267,7 +319,10 @@ describe('tephra', function() {
         test_secret,
         auth_request,
         function(err) {
-          if (err && !err.killed) return done(err)
+          if (err && !err.killed) {
+            done(err)
+            return
+          }
         }
       )
     })
@@ -283,7 +338,10 @@ describe('tephra', function() {
         test_secret,
         auth_request,
         function(err) {
-          if (err && !err.killed) return done(err)
+          if (err && !err.killed) {
+            done(err)
+            return
+          }
         }
       )
     })
@@ -297,28 +355,54 @@ describe('tephra', function() {
         test_secret,
         auth_request,
         function(err) {
-          if (err && !err.killed) return done(err)
+          if (err && !err.killed) {
+            done(err)
+            return
+          }
         }
       )
     })
 
-    it('#disconnect should throw if not given rinfo', function() {
+    it('should emit coa ack or nack and disconnect ack or nack when receiving a change of authorisation response', function(done) {
+      var emissions = 0
+      var expected = 4
+
+      function emission_counter() {
+        emissions += 1
+        if (emissions === expected) {
+          done()
+          return
+        }
+      }
+
+      server.on('CoA-NAK', emission_counter)
+      server.on('CoA-ACK', emission_counter)
+      server.on('Disconnect-NAK', emission_counter)
+      server.on('Disconnect-ACK', emission_counter)
+
+      server.send('authentication', 'CoA-ACK', {port: 1814, address: 'localhost'}, [], {}, function() {})
+      server.send('authentication', 'CoA-NAK', {port: 1814, address: 'localhost'}, [], {}, function() {})
+      server.send('authentication', 'Disconnect-ACK', {port: 1814, address: 'localhost'}, [], {}, function() {})
+      server.send('authentication', 'Disconnect-NAK', {port: 1814, address: 'localhost'}, [], {}, function() {})
+    })
+
+    it('should throw when disconnect is not given rinfo', function() {
       expect(
         server.disconnect.bind(server, null, [], {})
       ).to.throw()
     })
 
-    it('#disconnect should not throw if given all required arguments', function() {
+    it('should not throw when disconnect is given all required arguments', function() {
       expect(
         server.disconnect.bind(server, {address: '0.0.0.0', port: 12345}, [], {})
       ).to.not.throw
     })
 
-    it('#send should throw if supplied non-string type', function() {
+    it('should throw when send is supplied non-string type', function() {
       expect(server.send).to.throw(/string argument type/)
     })
 
-    it('#send should yield an error if supplied non-array type', function(done) {
+    it('should yield an error when send is supplied non-array type', function(done) {
       server.send(
         'accounting',
         0,
@@ -326,22 +410,23 @@ describe('tephra', function() {
         null,
         null,
         function(err) {
-          return done(
+          done(
             err ?
             null :
             new Error(
               'assertion failed: expected `err` to be truthy'
             )
           )
+          return
         }
       )
     })
 
-    it('#respond should throw if supplied non-string type', function() {
+    it('should throw when respond is supplied non-string type', function() {
       expect(server.respond).to.throw(/string argument type/)
     })
 
-    it('#respond should yield an error if no packet is given', function(done) {
+    it('should yield an error when respond is not given a packet type', function(done) {
       server.respond(
         'accounting',
         null,
@@ -350,13 +435,14 @@ describe('tephra', function() {
         null,
         null,
         function(err) {
-          return done(
+          done(
             err ?
             null :
             new Error(
               'assertion failed: expected `err` to be truthy'
             )
           )
+          return
         }
       )
     })
